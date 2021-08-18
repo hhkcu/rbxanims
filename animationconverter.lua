51bee2b21b59889c8cd039e9e92a43f85187f64d
problematic-scripter <72319973+problematic-scripter@users.noreply.github.com>
2021-08-18 10:05:50 +0100

    Create animationconverter.lua

---
+++ animationconverter.lua
@@ -0,0 +1,30 @@
+local toolbar = plugin:CreateToolbar("anim converter")
+local newScriptButton = toolbar:CreateButton("convert", "convert", "rbxassetid://7224540926")
+newScriptButton.ClickableWhenViewportHidden = true
+
+local selection = game:GetService("Selection")
+
+newScriptButton.Click:Connect(function()
+	local selected = selection:Get()[1]
+	if selected:IsA("KeyframeSequence") then
+		local kframes = {}
+		local tweenTime = 0
+		local ns = Instance.new("Script")
+		ns.Name = selected.Name
+		ns.Source = ""
+		for i,v in pairs(selected:GetDescendants()) do
+		if v:IsA("Keyframe") then
+			kframes[i] = v
+			pcall(function() tweenTime = kframes[i+1].Time - kframes[i].Time end)
+			ns.Source = ns.Source.."\nwait("..v.Time..")"
+		elseif v:IsA("Pose") then
+				if v.Name ~= "HumanoidRootPart" then
+					ns.Source = ns.Source.."\ntween(welds[\""..v.Name.."\"],"..tweenTime..","..tostring(v.EasingStyle)..","..tostring(v.EasingDirection)..",default[\""..v.Name.."\"]*CFrame.new("..tostring(v.CFrame).."))"
+				end
+			end
+		end
+		ns.Parent = game:GetService("ServerScriptService")
+	else
+		warn("anim converter: Selected object is not of class KeyframeSequence")
+	end
+end)
